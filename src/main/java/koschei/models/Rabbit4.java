c655d527b857607cf8c76eb1295df13e0efd9b70
yaroslav <ylatushkov@yahoo.com>
2022-12-25 17:04:04 +0300

    done 2.0!

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private final Duck5 duck;
+    private Duck5 duck;
 
-    @Autowired
-    public Rabbit4(Duck5 duck) {
-        this.duck = duck;
-    }
     @Override
     public String toString() {
         return ", в зайце утка " + duck.toString();
+    }
+
+    @Autowired
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
     }
 }
